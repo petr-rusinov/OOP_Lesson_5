9ed2503caa3ea69c3ffca45f8afee83039b028b9
petr-rusinov <petr-rusinov@yandex.ru>
2021-05-31 14:12:24 +0300

    minor fixes

--- OOP_Lesson_5/OOP_Lesson_5.cpp
+++ OOP_Lesson_5/OOP_Lesson_5.cpp
@@ -110,18 +110,21 @@ public:
 
 int main()
 {
-    //Pair1<int> p1(6, 9);
-    //cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
-
-    //const Pair1<double> p2(3.4, 7.8);
-    //cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
-
-    Pair<int, double> p1(6, 7.8);
+    cout << "Task 1" << endl;
+    Pair1<int> p1(6, 9);
     cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2(3.4, 7.8);
+    cout <<  "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    cout << endl << "Task 2" << endl;
+    Pair<int, double> p1_(6, 7.8);
+    cout << "Pair: " << p1_.first() << ' ' << p1_.second() << '\n';
     
-    const Pair<double, int> p2(3.4, 5);
-    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    const Pair<double, int> p2_(3.4, 5);
+    cout << "Pair: " << p2_.first() << ' ' << p2_.second() << '\n';
     
+    cout << endl << "Task 3" << endl;
     StringValuePair<int> svp("Amazing", 7);
     std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
 
