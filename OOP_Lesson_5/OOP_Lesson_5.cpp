e37026e64b51a0b5890f7cd647c3d3d967b25cfd
petr-rusinov <petr-rusinov@yandex.ru>
2021-06-02 13:10:11 +0300

    Black Jack classes fixed

--- OOP_Lesson_5/OOP_Lesson_5.cpp
+++ OOP_Lesson_5/OOP_Lesson_5.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 //1.
@@ -104,7 +105,82 @@ public:
 //Bust() - выводит на экран имя игрока и объявляет, что у него перебор.
 
 
-class GenericPlayer
+enum eSuit { CLUBS, SPADES, DIAMONDS, HEARTS }; //крести, пики, буби, черви
+enum eRank { ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
+
+class Card
+{
+    eSuit m_suit;
+    eRank m_rank;
+    bool m_isFaceUp;
+public:
+    Card(eSuit suit, eRank value, bool pos) : m_suit(suit), m_rank(value), m_isFaceUp(pos) {}
+    void flip() { m_isFaceUp = !m_isFaceUp; }
+    int getValue() 
+    {
+        int value = 0;
+        if (m_isFaceUp)
+        {
+            value = m_rank;
+            if (value > 10)
+            {
+                value = 10;
+            }
+        }
+        return value;
+    }
+};
+
+
+class Hand
+{
+private:
+    vector<Card*> m_cards; //колода карт
+public:
+    Hand() { m_cards.reserve(7); }
+    virtual ~Hand() { clear(); }
+    void add(Card* pCard) { m_cards.push_back(pCard); }
+    void clear() 
+    { 
+        vector<Card*>::iterator iter = m_cards.begin();
+        for (iter = m_cards.begin(); iter != m_cards.end(); ++iter)
+        {
+            delete *iter;
+            *iter = 0;
+        }
+        m_cards.clear();
+    }
+    int getTotal()
+    {
+
+        if (m_cards.empty()) 
+        {
+            return 0;
+        }
+        if (m_cards[0]->getValue() == 0)
+        {
+            return 0;
+        }
+
+        int s = 0;
+        int acesNum = 0;
+        for (Card* c : m_cards)
+        {
+            int val = c->getValue();
+            if (val == ACE) acesNum++;
+            s += val;
+        }
+        if (acesNum < 2) //Если вдруг тузов больше 1, нет смысла их считать как 11
+        {
+            if ((s + 10) <= 21) //Пробуем считать туз как 11, поэтому добавляем к общей сумме 10
+                s += 10;
+        }
+        return s;
+    }
+};
+
+
+class GenericPlayer : public Hand
 {
 private:
     string m_playerName;
@@ -112,8 +188,9 @@ public:
     virtual bool isHitting() = 0;
     bool isBoosted() 
     {
-        return false;
-    };
+        return getTotal() > 21;
+    }
+
     void bust()
     {
         cout << m_playerName << " busted" << endl;
@@ -142,6 +219,17 @@ int main()
     StringValuePair<int> svp("Amazing", 7);
     std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
 
+    Hand hand;
+    Card *c1 = new Card(SPADES, NINE, true);
+    Card *c2 = new Card(SPADES, TWO, true);
+    Card *c3 = new Card(SPADES, ACE, true);
+
+    hand.add(c1);
+    hand.add(c2);
+    hand.add(c3);
+
+    int v = hand.getTotal();
+    cout << "Hand total score: " << v << endl;
 
     return 0;
 }
